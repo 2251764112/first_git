82beb721ea2a99c09fd38aadfda0ffb951d7f19b
TDJ <TDJ@DESKTOP-OCIDSQU>
2020-03-03 10:45:03 +0800

    修改分支的修改内容

--- src/servlet/UserServlet.java
+++ src/servlet/UserServlet.java
@@ -38,7 +38,7 @@ public class UserServlet extends HttpServlet {
 		//页面上获得的所有数据都是string类型，而年龄是int类型
 		//所以这里我们需要对这些数据进行强转（包装类）
 		int age=Integer.parseInt(req.getParameter("age"));
-		user.setName(name);
+		user.setName("张三");
 		//调用实体类中的setter方法为私有的属性赋值
 		user.setAge(20);
 		
